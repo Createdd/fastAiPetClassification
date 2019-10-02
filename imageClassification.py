cbf1c67b3f770f253668513ed822a9ecfc837733
Daniel Deutsch <deudan1010@gmail.com>
2019-10-02 14:40:04 +0200

    format according to black

--- imageClassification.py
+++ imageClassification.py
@@ -2,15 +2,23 @@
 from fastai import *
 from fastai.vision import *
 
-batchsize=64
-path=untar_data('./data/oxford-iiit-pet')
-annotationPath=path/'annotations'
-imagePath=path/'images'
+batchsize = 64
+path = untar_data("./data/oxford-iiit-pet")
+annotationPath = path / "annotations"
+imagePath = path / "images"
 
-fileNames=get_image_files(imagePath)
-regexPattern=re.compile(r'/([^/]+)_\d+.jpg$')
+fileNames = get_image_files(imagePath)
+regexPattern = re.compile(r"/([^/]+)_\d+.jpg$")
 
-data=ImageDataBunch.from_name_re(imagePath, fileNames, regexPattern, ds_tfms=get_transforms(), size=224, bs=batchsize, num_workers=0).normalize(imagenet_stats)
+data = ImageDataBunch.from_name_re(
+    imagePath,
+    fileNames,
+    regexPattern,
+    ds_tfms=get_transforms(),
+    size=224,
+    bs=batchsize,
+    num_workers=0,
+).normalize(imagenet_stats)
 
-print('printtest')
+print("printtest")
 
